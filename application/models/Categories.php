47dec9d05e287c40f95b4923bf0848d71a5d7b09
Jason Chen <jasonccmfry@gmail.com>
2018-02-08 13:38:31 -0800

    update changelog and comments

--- application/models/Categories.php
+++ application/models/Categories.php
@@ -1,11 +1,16 @@
 <?php
 class Categories extends CSV_Model
 {
-
+    // categories Id
     public $id;
+    // categories name
     public $name;
+    // categories description
     public $description;
 
+    /**
+     * Contructor for Categories
+     */
     function __construct()
     {
         parent::__construct("data/Categories.csv", "id", "entity");
