c47f7dda14e00c6587539e4976e1b894e0240d29
Justin-Leung <lngventure@gmail.com>
2018-02-08 16:21:30 -0800

    Implemented the menubar into its partial, and loaded the menu links/text from the config application/config/config.php file.

--- application/views/_menubar.php
+++ application/views/_menubar.php
@@ -1,5 +1,17 @@
-<ul class="nav">
-    {menudata}
-    <li><a href="{link}">{name}</a></li>
-    {/menudata}
-</ul>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Accessorizer</a>
+
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            {menudata}
+                <li class="nav-item">
+                    <a class="nav-link" href="{link}">{name}</a>
+                </li>
+            {/menudata}
+        </ul>
+    </div>
+</nav>
