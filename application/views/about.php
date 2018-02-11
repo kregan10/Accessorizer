7c4bdc8220517b367fd2d22deec32d5e271438b3
Kerry Regan <kregan89@gmail.com>
2018-02-11 13:54:22 -0800

    updated styling of about page

--- application/views/about.php
+++ application/views/about.php
@@ -2,14 +2,14 @@
 
   <p class="text-center mx-auto about-text">
 
-    Tired of running around the battlefield in your underwear naked and afraid?  Well
-    you'ce come to the right place. You've made your way to our PUBG accessorizor simulator!  This is purely for
+    Tired of running around the battlefield in your underwear <strong>naked</strong> and <strong>afraid?</strong>  Well
+    you've come to the right place. You've made your way to our PUBG accessorizor simulator!  This is purely for
     entertainment purposes and please note that none of the stats of any of the accessories
     reflect the actual stats in PUBG.
     <hr/>
   </p>
   <h2>Happy Accessorizing</h2>
-  <p>From,</p>
-  <p>The Accessorizer Team</p>
+  <h3>From,</h3>
+  <h3>The Accessorizer Team</h3>
 
 </div>
