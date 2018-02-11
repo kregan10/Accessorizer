8cea36b6f93eaafabbc306c31a3498806d612137
Hamish Brindle <hamishbrindle@gmail.com>
2018-02-10 19:49:58 -0800

    Tweaked some CSS and added a background for the body

--- application/views/_menubar.php
+++ application/views/_menubar.php
@@ -1,5 +1,5 @@
-<nav class="navbar navbar-expand-md navbar-light" id="menubar">
-    <a class="navbar-brand" href="/">{menubrand}</a>
+<nav class="navbar navbar-expand-md navbar-dark" id="menubar">
+    <a class="navbar-brand violence" href="/">{menubrand}</a>
 
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
