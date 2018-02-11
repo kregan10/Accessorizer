66a063944d7a61fc4ae3436fd1a32515cbab6cf4
HMilbradt <harrison.milbradt@gmail.com>
2018-02-11 09:46:55 -0800

    Fixed styling on footer, centered, and stuck to bottom.  Changed base application name in MY_COntroller

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -23,7 +23,7 @@ class Application extends CI_Controller
 
 		//  Set basic view parameters
 		$this->data = array ();
-		$this->data['pagetitle'] = 'TODO List Manager';
+		$this->data['pagetitle'] = 'PUBG Kit Selector';
 		$this->data['ci_version'] = (ENVIRONMENT === 'development') ? 'CodeIgniter Version <strong>'.CI_VERSION.'</strong>' : '';
 	}
 
--- application/views/_footer.php
+++ application/views/_footer.php
@@ -1,3 +1,3 @@
-<footer class="text-center">
-	<p>&copy; {footerbrand} {date}</p>
+<footer class="text-center mx-auto">
+	<h3>&copy; {footerbrand} {date}</h3>
 </footer>
